a856ce730604d44949a817b0c059737bd3b32ec0
Aykut Asil <aykuttasil@gmail.com>
2020-05-10 23:15:31 +0300

    firebase ios configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,10 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fireter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -28,3 +27,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+*/

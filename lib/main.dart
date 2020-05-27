057df879508deec3b7d67e6a7a2fca48b607552f
Aykut Asil <aykuttasil@gmail.com>
2020-05-27 23:31:31 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
+
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
